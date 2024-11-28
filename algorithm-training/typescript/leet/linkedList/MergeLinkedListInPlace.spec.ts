13e3865df2f74ccdf9abc820e7030345ca55ea00
color <jgstringer@gmail.com>
2024-11-28 17:46:11 -0600

    reduced lines of code for readability

---
+++ algorithm-training/typescript/leet/linkedList/MergeLinkedListInPlace.spec.ts
@@ -0,0 +1,49 @@
+import { mergeLinkedListInPlace } from './MergeLinkedListInPlace';
+
+describe('MergeLinkedListInPlace', () => {
+    it('should return [1,1,2,3,4,4] when l1 = [1,2,4], l2 = [1,3,4]', () => {
+        const l1 = { val: 1, next: { val: 2, next: { val: 4, next: null } } };
+        const l2 = { val: 1, next: { val: 3, next: { val: 4, next: null } } };
+        const expected = {
+            val: 1,
+            next: {
+                val: 1,
+                next: {
+                    val: 2,
+                    next: {
+                        val: 3,
+                        next: { val: 4, next: { val: 4, next: null } }
+                    }
+                }
+            }
+        };
+        expect(mergeLinkedListInPlace(l1, l2)).toEqual(expected);
+    });
+
+    it('should return [1,2,3] when l1 = [1,2,3], l2 = []', () => {
+        const l1 = { val: 1, next: { val: 2, next: { val: 3, next: null } } };
+        const l2 = null;
+        const expected = {
+            val: 1,
+            next: { val: 2, next: { val: 3, next: null } }
+        };
+        expect(mergeLinkedListInPlace(l1, l2)).toEqual(expected);
+    });
+
+    it('should return [1,2,3] when l1 = [], l2 = [1,2,3]', () => {
+        const l1 = null;
+        const l2 = { val: 1, next: { val: 2, next: { val: 3, next: null } } };
+        const expected = {
+            val: 1,
+            next: { val: 2, next: { val: 3, next: null } }
+        };
+        expect(mergeLinkedListInPlace(l1, l2)).toEqual(expected);
+    });
+
+    it('should return [] when l1 = [], l2 = []', () => {
+        const l1 = null;
+        const l2 = null;
+        const expected = null;
+        expect(mergeLinkedListInPlace(l1, l2)).toEqual(expected);
+    });
+});
