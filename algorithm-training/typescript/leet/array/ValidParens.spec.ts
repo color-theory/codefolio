18370201affbc3291dcf10ca3fab1b40b55e6e63
color <jgstringer@gmail.com>
2024-11-27 21:58:53 -0600

    validate parens

---
+++ algorithm-training/typescript/leet/array/ValidParens.spec.ts
@@ -0,0 +1,38 @@
+import { validateParens } from "./ValidParens";
+
+describe("validateParens", () => {
+  it("should return true when s = '()'", () => {
+    const s = "()";
+    expect(validateParens(s)).toEqual(true);
+  });
+
+  it("should return true when s = '()[]{}'", () => {
+    const s = "()[]{}";
+    expect(validateParens(s)).toEqual(true);
+  });
+
+  it("should return false when s = '(]'", () => {
+    const s = "(]";
+    expect(validateParens(s)).toEqual(false);
+  });
+
+  it("should return false when s = '([)]'", () => {
+    const s = "([)]";
+    expect(validateParens(s)).toEqual(false);
+  });
+
+  it("should return true when s = '{[]}'", () => {
+    const s = "{[]}";
+    expect(validateParens(s)).toEqual(true);
+  });
+
+  it("should return false when s = '}'", () => {
+    const s = "}";
+    expect(validateParens(s)).toEqual(false);
+  });
+
+  it("should return true when s = ''", () => {
+    const s = "";
+    expect(validateParens(s)).toEqual(true);
+  });
+});
---
+++ algorithm-training/typescript/leet/array/ValidParens.ts
@@ -0,0 +1,22 @@
+export const validateParens = (s: string) => {
+  const openers = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+  let opened = [];
+  for (let i = 0; i < s.length; i++) {
+    let nextChar = s[i];
+    if (nextChar == "(" || nextChar == "{" || nextChar == "[") {
+      opened.push(nextChar);
+    } else if (nextChar == ")" || nextChar == "}" || nextChar == "]") {
+      if (opened[opened.length - 1] == openers[nextChar]) {
+        opened.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return opened.length === 0;
+};
